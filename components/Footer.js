aa04feca0fdeed1848113fe68ad9cba595306dfe
Oğuz <53166823+oguzdurmaz-dev@users.noreply.github.com>
2021-05-23 17:34:06 +0300

    Add files via upload
    Footer - switched fixed -> absolute

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function FooterPage() {
   return (
     <div
       className="flex justify-center items-center p-1 bg-gray-800 text-gray-200 mt-10 w-full absolute "
-      style={{ 'bottom': "0" }}
+      style={{ 'bottom': "0" , "position": "absolute"}}
     >
       <h2 style={{ "fontSize": "13px" }}>
         Chatlify 1.0 - Built by Oguz Durmaz
