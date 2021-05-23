49b31bac84db3839f3d3801694b650c2cfb4d4da
Oğuz <53166823+oguzdurmaz-dev@users.noreply.github.com>
2021-05-23 21:05:30 +0300

    Add files via upload

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,14 +1,21 @@
 import Button from "../components/Button";
 import Image from "next/image";
+import Head from "next/head";
 import styles from "../styles/SignIn.module.css";
 import FooterPage from "../components/Footer";
 export default function SignInPage({
   signInWithGoogle,
-
+  title,
+  keywords,
+  description,
 }) {
   return (
     <div className="h-screen bg-gray-300">
-
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
       <div className={styles.signInContainer}>
         <Image src="/images/chatlify_logo.png" width="200" height="60" />
         <p>The easiest way to chat with people all around the world.</p>
@@ -21,3 +28,10 @@ export default function SignInPage({
   );
 }
 
+SignInPage.defaultProps = {
+  title:
+    "Chattify 1.0 | The easiest way to chat with people all around the world.",
+  description: "Talk the buddies around the world!",
+  keywords:
+    "nextjs chat, nextjs firebase chat, nextjs chat, nextjs messenger, tailwind nextjs messenger",
+};
