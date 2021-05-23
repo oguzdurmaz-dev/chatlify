39ea4a5cbabd82edeb0b6a9d6967b894c21fa883
Oğuz <53166823+oguzdurmaz-dev@users.noreply.github.com>
2021-05-23 17:30:07 +0300

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 export default function FooterPage() {
   return (
     <div
-      className="flex justify-center items-center p-1 bg-gray-800 text-gray-200 mt-10 w-full"
-      style={{ 'marginTop': "45px" }}
+      className="flex justify-center items-center p-1 bg-gray-800 text-gray-200 mt-10 w-full absolute "
+      style={{ 'bottom': "0" }}
     >
       <h2 style={{ "fontSize": "13px" }}>
         Chatlify 1.0 - Built by Oguz Durmaz
